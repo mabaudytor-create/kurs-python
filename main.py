0c463659e63b7c12ebe469ee63f1ca811e41e5fe
AUDYTOR <audytor@audytor.biz.pl>
2026-02-23 17:55:42 +0100

    add gitignore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # This is a sample Python script.
-git
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -15,3 +14,5 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print("Witaj świecie")
+
+print("hallo")
